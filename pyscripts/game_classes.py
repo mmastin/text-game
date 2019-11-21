f7ef7cebf58bd9c3c248721a79bcebc5ebf61811
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-11-21 08:05:48 -0500

    backend stuff

---
+++ pyscripts/game_classes.py
@@ -0,0 +1,30 @@
+class room:
+    def __init__(self, location):
+        self.location = location
+        self.occupied = False
+        
+class gameMap:
+    def __init__(self):
+        self.map = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.rooms = []
+    
+    def generate_rooms():
+        for x in range(len(self.map)):
+            r = room(x)
+            self.rooms.append(r)
+
+
+
+class Player:
+    def __init__(self, location, id_):
+        self.location = location
+        self.id_ = id_
