6bf2fc1ea2509aaea2a5204941eae99638158a73
mmastin <matthew.mastin@gmail.com>
2019-11-22 07:44:38 -0500

    v1 full working game

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import random
 from pyscripts.game_classes import *
-from pyscripts.database_class import *
 
 
 gmap = gameMap()
+gmap.generate_rooms()
 num_players = 0
 players = []
-
-connector = dbConnector()
+potions = ["(^)", "(+)", "(!)"]
+bad_norths = [x for x in range(0, 10)]
+bad_souths = [x for x in range(90, 100)]
+bad_easts = [x for x in range(9, 100, 10)]
+bad_wests = [x for x in range(0, 100, 10)]
+possible_directions = {"n": -10, "s": 10, "e": 1, "w": -1}
+deaths = 0
 
 # Create flask app
 app = Flask(__name__)
@@ -21,12 +26,13 @@ socketio = SocketIO(app)
 # Start Screen
 @app.route('/')
 def start():
-    return render_template('start.html')
+    return render_template('start.html', data = deaths)
 
 # Game route
 @app.route('/game')
 def game():
     return render_template('game.html')
+
 
 # Callback function to aid in debugging
 def messageReceived(data, methods=['GET', 'POST']):
@@ -43,69 +49,157 @@ def custom_event(data, methods=['GET', 'POST']):
         global num_players
         # create an id for the new player 
         id_ = num_players + 1
-        # Set a random spawn location
-        position = random.randint(0, 100)
-        # create player object with position and id
-        player = Player(position, id_) 
+        # checks map for available spots
+        available = [index for index in range(len(gmap.map)) if gmap.map[index] == 0]
+        try:
+            # Set a random spawn location
+            position = random.choice(available)
+        except:
+            # if room is filled, send user back to start screen
+            return render_template('game.html')
+        #create player object with position and id
+        player = Player(position, id_)
         # append new player to players
         players.append(player)
         # Update gamemap to include player
         gmap.map[player.location] = player.id_
         # Increment amount of players by 1
         num_players += 1
-        message = f"Player {id_} has joined the dungeon"
+        message = " has joined the dungeon"
         # Create the data that will be sent back(map cords and players id)
         data = {"data": gmap.map, "player": player.id_, "message": message}
         # sent client data
         return socketio.emit('my response', data)
-    # Runs if user sent a message
-    if data["message"] != '':
-        data = {"data": gmap.map, "player": data["player"], "message": data["message"]}
-        socketio.emit('my response', data)
     # Runs if client is already connected
     else:
         # Grab the players id from data recieved
         id_ = int(data["player"])
-        # Check direction inputed
-        direction = data["data"]
-        # Check players current position on map
-        position = gmap.map.index(id_)
-        # runs if input direction is invalid
-        if direction.lower() not in ["forward", "backward", "f", "b"]:
-            data = {"data": gmap.map, "player": id_}
-            socketio.emit('my response', data)
-        # Runs if input position is valid and they are in the first room
-        if position == 0:
-            # if they are moving forward
-            if direction.lower() in ["forward", "f"]:
-                # Change map accordingly
-                gmap.map[position] = 0
-                gmap.map[position+1] = id_
-            # Create data and send to client
-            data = {"data": gmap.map, "player": id_}
-            socketio.emit('my response', data)
-        # Runs if player isn't in the first or last room
-        elif (position > 0) and (position <= 98):
-            # Runs if they are moving forward
-            if direction.lower() in ["forward", "f"]:
-                gmap.map[position] = 0
-                gmap.map[position+1] = id_
-            # Runs if they are moving backward
-            if direction.lower() in ["backward", "b"]:
-                gmap.map[position] = 0
-                gmap.map[position-1] = id_
-            data = {"data": gmap.map, "player": id_}
-            socketio.emit('my response', data)
-        # Runs if player is in last room
+        if players[id_-1].health <= 0:
+            return render_template('end.html')
+        # Runs if user sent a message
+        if data["message"] != '':
+            data = {"data": gmap.map, "player": data["player"], "message": data["message"]}
+            return socketio.emit('my response', data)
+        # Runs if user is moving
         else:
-            # Runs if they want to go backwards
-            if direction.lower() in ["backward", "b"]:
-                gmap.map[position] = 0
-                gmap.map[position+1] = id_
-            data = {"data": gmap.map, "player": id_}
-            socketio.emit('my response', data)
+            # Check direction inputed
+            direction = data["data"].lower()
+            # Check players current position on map
+            position = gmap.map.index(id_)
+            # run if direction is valid
+            if direction in possible_directions:
+                # checks map for available spots
+                available = [index for index in range(len(gmap.map)) if gmap.map[index] == 0]
+                chance = random.randint(0, 100)
+                random_spawn = random.choice(available)
+                if chance <= 20:
+                    gmap.map[random_spawn] = "(+)"
+                elif (chance > 20) and (chance <= 40):
+                    gmap.map[random_spawn] = "(^)"
+                elif (chance > 40) and (chance <= 60):
+                    gmap.map[random_spawn] = "(!)"
+                elif (chance == 99):
+                    gmap.map[random_spawn] = "\***/"
+                else:
+                    pass
+                # create new position
+                new_position = position + possible_directions[direction]
+                # runs new position is on the map
+                if (new_position >= 0) and (new_position <= 99):
+                    # Runs if room has an enemy
+                    if (new_position not in available) and (gmap.map[new_position] not in potions):
+                        # grabs the enemies id
+                        enemy_id = gmap.map[new_position]
+                        # decreses enemies health by player strength
+                        players[enemy_id -1].health -= players[id_-1].strength
+                        enemy_health = players[enemy_id-1].health
+                        # runs if the enemy died
+                        if players[enemy_id-1].health <= 0:
+                            # players current room is empty
+                            gmap.map[position] = 0
+                            # player takes enemies room
+                            gmap.map[new_position] = id_
+                            global deaths
+                            deaths += 1
+                            event = f" has killed Player {enemy_id}"
+                            data = {"data": gmap.map, "player": id_, "event": event}
+                            return socketio.emit('my response', data)
+                        # runs if they are alive
+                        else:
+                            # grabs enemy position
+                            enemy_position = new_position
+                            # all random choices
+                            flee_to = random.choice(available)
+                            # check if it works
+                            gmap.map[position] = 0
+                            gmap.map[enemy_position] = id_
+                            gmap.map[flee_to] = enemy_id
+                            event = f" has attacked Player {enemy_id}, causing them to flee with {enemy_health} health remaining"
+                            data = {"data": gmap.map, "player": id_, "attack": 1, "event": event}
+                            return socketio.emit('my response', data)
+                    # Runs if health potion was found
+                    elif (gmap.map[new_position] == "(+)"):
+                        gmap.map[position] = 0
+                        gmap.map[new_position] = id_
+                        players[id_ - 1].health += 20
+                        event = f" has found a health potion. Their health has rose to {players[id_ -1].health}"
+                        data = {"data" : gmap.map, "player": id_, "event": event}
+                        return socketio.emit('my response', data)
+                    elif (gmap.map[new_position] == "(^)"):
+                        gmap.map[position] = 0
+                        gmap.map[new_position] = id_
+                        players[id_ - 1].strength += 20
+                        event = f" has found a strength potion. Their strength has rose to {players[id_ -1].strength}"
+                        data = {"data" : gmap.map, "player": id_, "event": event}
+                        return socketio.emit('my response', data)
+                    elif (gmap.map[new_position] == "(!)"):
+                        gmap.map[position] = 0
+                        gmap.map[new_position] = id_
+                        players[id_ - 1].health -= 20
+                        event = f" has stepped on a trap. Their remaining health is {players[id_ -1].health}"
+                        data = {"data" : gmap.map, "player": id_, "event": event}
+                        return socketio.emit('my response', data)
+                     # Runs if room is empty
+                    else:
+                        # if the direction the user chose was north, and they are on the top of the map
+                        if (direction == "n") and (position in bad_norths):
+                            # return current map
+                            event = " has attempted to escape the dungeon"
+                            data = {"data": gmap.map, "player": id_, "event": event}
+                            return socketio.emit('my response', data)
+                        # if the direction is south and they are on the bottom of the map
+                        elif (direction == "s") and (position in bad_souths):
+                            event = " has attempted to escape the dungeon"
+                            data = {"data": gmap.map, "player": id_, "event": event}
+                            return socketio.emit('my response', data)
+                        # if the direction they chose was west and they are on the left of the map
+                        elif (direction == "w") and (position in bad_wests):
+                            event = " has attempted to escape the dungeon"
+                            data = {"data": gmap.map, "player": id_, "event": event}
+                            return socketio.emit('my response', data)
+                        # if the direction is east and they are on the right of the map
+                        elif (direction == "e") and (position in bad_easts):
+                            event = " has attempted to escape the dungeon"
+                            data = {"data": gmap.map, "player": id_, "event": event}
+                            return socketio.emit('my response', data)
+                        else:
+                            gmap.map[position] = 0
+                            gmap.map[new_position] = id_
+                            current_room = gmap.rooms[new_position]
+                            event = f" has moved to {current_room.name}"
+                            data = {"data": gmap.map, "player": id_, "event":event}
+                            return socketio.emit('my response', data)
+                # Runs if new position puts player off map
+                else:
+                    event = " has attempted to escape the dungeon"
+                    data = {"data": gmap.map, "player": id_, "event": event}
+                    return socketio.emit('my response', data)
+            # If the input direction isn't valid
+            else:
+                event = " has attempted to escape the dungeon"
+                data = {"data": gmap.map, "player": id_, "event": event}
+                return socketio.emit('my response', data)
+                
     
-
-
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- pyscripts/game_classes.py
+++ pyscripts/game_classes.py
@@ -1,8 +1,24 @@
+import random
+
 class room:
     def __init__(self, location):
         self.location = location
         self.occupied = False
+        self.name = None
         
+    def room_name_generator(self):
+        first_words = ['Firey', 'Whispering', 'Ancient', 'Musty',
+        'Bloody', 'Treasure', 'Green', 'Babbling',
+        'Creaky', 'Freezing', 'Dank', 'Mysterious', 
+        'Magical', 'Wandering', 'Deathly']
+        second_words = ['Hobgloblin', 'Orc', 'Centaur', 'Dwarf',
+        'Goliath', 'Gnome', 'Dragon', 'Emerald',
+        'Wizard', 'Hearthstone', 'Warlock', 'Aragorn',
+        'Gollum', 'Gimli', 'Saruman', 'Balrog',
+        'Cellar', 'Chamber', 'Wight']
+        rand_name = random.choice(first_words) + ' ' + random.choice(second_words)
+        self.name = rand_name
+
 class gameMap:
     def __init__(self):
         self.map = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -17,14 +33,16 @@ class gameMap:
                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.rooms = []
     
-    def generate_rooms():
+    def generate_rooms(self):
         for x in range(len(self.map)):
             r = room(x)
+            r.room_name_generator()
             self.rooms.append(r)
-
 
 
 class Player:
     def __init__(self, location, id_):
         self.location = location
         self.id_ = id_
+        self.health = 100
+        self.strength = 20
